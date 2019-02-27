1951b6cd60ff8c5da691b5b9ae7ad6b25813fbdc
Vukosavljev <vukosavljevs@gmail.com>
2019-02-27 22:29:02 +0100

    basic setup and display

---
+++ src/containers/ClassromContainer.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import Student from '../components/Student'
+
+export default class ClassromContainer extends Component {
+    state = {
+        students: [
+            {name: 'Marko', id: 1}, {name: 'Pera', id: 2}, {name: 'Mika', id: 3},
+            {name: 'Perica', id: 4}, {name: 'Aca', id: 5}, {name: 'Milos', id: 6},
+            {name: 'Marina', id: 7}, {name: 'Ana', id: 8}, {name: 'Milena', id: 9}, 
+            {name: 'Dragana', id: 10}, {name: 'Milica', id: 11}, {name: 'Ivana', id: 12}
+        ]
+    }
+
+    addAttendaceHandler = (e, id) => {
+        const attendStudents = [...this.state.students].map( student => {
+            if (student.id === id) {
+                student.attendace = e;
+            }
+            return student;
+        })
+        this.setState({ student: attendStudents });
+        console.log(attendStudents)
+    }
+
+    
+  render() {
+    const students = this.state.students.map( student => 
+        <Student key={student.id}
+        name={student.name}
+        attendace={student.attendace}  
+        addAttendace={(e) => this.addAttendaceHandler(e.target.alt, student.id)}
+        />)
+
+    return (
+      <div style={{display: 'flex', flexWrap:'wrap'}} >
+        {students}
+      </div>
+    )
+  }
+}
+
+
